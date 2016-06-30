d9b6ce2a7eaa94ca32f7c31fb9d8f56ce0ad1456
xinxin <xinxin@rrkd.cn>
2016-06-30 14:29:11 +0800

    edit the code

--- test.js
+++ test.js
@@ -42,24 +42,20 @@ client.on('end',function(err){
     console.log('end');
 });
 
-client.on('ready',function(err){
+client.on('connect',function(){
     var key = 'skills';
-    client.sadd(key,'C#','java',redis.print);
-    client.sadd(key,'nodejs');
-    client.sadd(key,'MySQL');
-
-    client.multi()
-    .sismember(key,'C#')
-    .sismember(key)
-    .exec(function(err,replies){
-        console.log("MULTI got "+ replies.length + "replies");
-        replies.forEach(function(reply, inedx){
-            console.log("Reply " + index + ": " + reply.toString());
-        });
-        client.quit();
+      client.sadd(key, 'C#','java',redis.print);
+      client.sadd(key, 'nodejs');
+      client.sadd(key, "MySQL");
+      
+      client.multi()      
+      .sismember(key,'C#')
+      .smembers(key)
+      .exec(function (err, replies) {
+            console.log("MULTI got " + replies.length + " replies");
+            replies.forEach(function (reply, index) {
+                console.log("Reply " + index + ": " + reply.toString());
+            });
+            client.quit();
     });
-});
-
-client.on('ready',function(err){
-    console.log('ready');
 });
